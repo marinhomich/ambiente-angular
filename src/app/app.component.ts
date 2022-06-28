0f54d2badb7ed42b22694a713f8866d5c1a4403a
Michel Marinho <michel.marinho04@outlook.com>
2022-06-28 09:28:32 -0400

    Iteração com ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   count = 0;
+  
+  pessoas = [
+    {
+      nome: "Michel",
+      sobrenome: "Marinho",
+    },
+    {
+      nome: "Everton",
+      sobrenome: "Gonçalves",
+    },
+    {
+      nome: "Adriano",
+      sobrenome: "Lopes",
+    },
+    {
+      nome: "Bruno",
+      sobrenome: "Campelo",
+    }
+  ]
 
   constructor(){
 
   }
 
   ngOnInit(): void {
+    console.log(this.pessoas);
     let interval = setInterval(() => {
       this.count++;
       if(this.count === 10){
